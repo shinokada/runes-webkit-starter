3b686f355c52f840dc4016e76371475b3f35c211
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-05 20:50:02 +0200

    first commit

---
+++ src/lib/index.ts
@@ -0,0 +1,6 @@
+export { default as A1Password } from './A1Password.svelte';
+export { default as Acast } from './Acast.svelte';
+export { default as Access } from './Access.svelte';
+export { default as Activitypub } from './Activitypub.svelte';
+export { default as Adobe } from './Adobe.svelte';
+export { default as Airbnb } from './Airbnb.svelte';
---
+++ src/routes/icons/three-tabs.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import type { ComponentType } from 'svelte';
+  import { IconPage, filterIconsByKeyword } from 'svelte-icon-webkit';
+  import * as icons from '$lib'
+  const keywordsToInclude = 'Solid';
+  const keyIcons = filterIconsByKeyword(icons, keywordsToInclude);
+</script>
+
+<IconPage icons={keyIcons as ComponentType} title="Three Tabs" />
