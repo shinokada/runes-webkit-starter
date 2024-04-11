0bafec020adedc5c9726249c3c8fe7a069e84536
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-11 20:16:27 +0200

    chore: update dependencies

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,67 +1,33 @@
-import type { MetaProps } from '$lib'
+import type { MetaProps } from 'runes-meta-tags'
 import { ANALYTICS_ID } from '$env/static/private';
 
 
-// export interface TwitterType {
-//   card?: 'summary' | 'summary_large_image' | 'app' | 'player';
-//   site?: string;
-//   handle?: string;
-//   title?: string;
-//   description?: string;
-//   image?: string;
-//   imageAlt?: string;
-// }
-// export interface OgType{
-//   type?: string;
-//   title?: string;
-//   description?: string;
-//   url?: string;
-//   image?: string;
-//   imageAlt?: string;
-//   siteName?: string;
-//   imageWidth?: string;
-//   imageHeight?: string;
-// }
-// export interface MetaProps {
-//   title?: string;
-//   robots?: boolean;
-//   description?: string;
-//   keywords?: string;
-//   twitter?: TwitterType;
-//   og?: OgType;
-//   url?: string;
-//   imageWidth?: string;
-//   imageHeight?: string;
-// }
 export const load = ({ url }) => {
   const layoutMetaTags = Object.freeze({
-    title: 'Runes meta',
-    description: 'Meta tags for Runes.',
-    keywords: 'runes, meta, tags',
-
+    title: 'Runes webkit starter',
+    description: 'Webkit starter for Svelte Runes',
+    keywords: 'runes, starter, web',
     twitter: {
       card: 'summary_large_image',
       site: '@shinokada',
       handle: '@shinokada',
-      title: 'Runes meta',
-      description: 'Meta tags for Runes.',
-      image: 'https://open-graph-vercel.vercel.app/api/runes-meta',
+      title: 'Runes webkit starter',
+      description: 'Webkit starter for Runes.',
+      image: 'https://open-graph-vercel.vercel.app/api/runes-webkit-starter',
       imageAlt: 'Runes meta',
     },
 
     og: {
       type: 'website',
-      title: 'Runes meta',
-      description: 'Meta tags for Runes.',
+      title: 'Runes webkit starter',
+      description: 'Webkit starter for Svelte Runes.',
       url: url.href,
-      image: 'https://open-graph-vercel.vercel.app/api/runes-meta',
-      imageAlt: 'Runes meta',
-      siteName: 'Runes meta',
+      image: 'https://open-graph-vercel.vercel.app/api/runes-webkit-starter',
+      imageAlt: 'Runes webkit starter',
+      siteName: 'Runes webkit starter',
       imageWidth: '1200',
       imageHeight: '630',
     }, 
-
-
   }) satisfies MetaProps;
 
   return {
