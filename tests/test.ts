c8b5717ebaeadcb488091f97959d908231acc57e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-20 14:27:02 +0200

    fix: guide pages

--- tests/test.ts
+++ tests/test.ts
@@ -7,17 +7,17 @@ test('svelte-4/getting-started page has expected h1', async ({ page }) => {
 
 test('svelte-4/props page has expected h1', async ({ page }) => {
   await page.goto('/guide/svelte-4/props');
-  await expect(page.locator('h1')).toHaveText('Svelte Awesome Icons v1: Props');
+  await expect(page.locator('h1')).toHaveText('Runes Webkit v1: Props');
 });
 
 test('svelte-5/getting-started page has expected h1', async ({ page }) => {
   await page.goto('/guide/svelte-5/getting-started');
-  await expect(page.locator('h1')).toHaveText('Svelte Awesome Icons: v2');
+  await expect(page.locator('h1')).toHaveText('Runes Webkit: v2');
 });
 
 test('svelte-5/props page has expected h1', async ({ page }) => {
   await page.goto('/guide/svelte-5/props');
-  await expect(page.locator('h1')).toHaveText('Props: Svelte Ant Design Icons v2 for Svelte Runes');
+  await expect(page.locator('h1')).toHaveText('Props: Runes Webkit v2 for Svelte Runes');
 });
 
 test('custom-icon page has expected h1', async ({ page }) => {
@@ -28,24 +28,4 @@ test('custom-icon page has expected h1', async ({ page }) => {
 test('global-icon page has expected h1', async ({ page }) => {
   await page.goto('/guide/global-icons');
   await expect(page.locator('h1')).toHaveText('Setting Global Icon using setContext');
-});
-
-test('three-tabs page has expected h1', async ({ page }) => {
-  await page.goto('/three-tabs');
-  await expect(page.locator('h1')).toHaveText('Three Tabs');
-});
-
-test('three-tabs-sizebytailwind page has expected h1', async ({ page }) => {
-  await page.goto('/three-tabs-sizebytailwind');
-  await expect(page.locator('h1')).toHaveText('Three Tabs: Size by Tailwind');
-});
-
-test('no-tabs page has expected h1', async ({ page }) => {
-  await page.goto('/no-tabs');
-  await expect(page.locator('h1')).toHaveText('No Tabs');
-});
-
-test('how to use page has expected h1', async ({ page }) => {
-  await page.goto('/how-to-use');
-  await expect(page.locator('h1')).toHaveText('Documentation');
 });
