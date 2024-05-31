903d0435be87770a480daff5b79e1faaf62db9bd
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-31 06:46:31 +0200

    tests: add tests to icons page

---
+++ src/routes/icons/+page.ts
@@ -0,0 +1,23 @@
+import type { MetaProps } from 'runes-meta-tags';
+
+const title = 'Icons - Svelte Remix'
+const description = 'Easily Find & Copy SVG Icon name for Your Project'
+const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-remix'
+
+export const load = ({ url }) => {
+  const pageMetaTags = Object.freeze({
+    title,
+    description,
+    og: {
+      title,
+      description,
+      image: imgUrl
+    },
+    twitter: {
+      title,
+      description,
+      image: imgUrl
+    }
+  }) satisfies MetaProps;
+  return { pageMetaTags };
+};
