cb43d9d843b253af6ad3f18e8aeba1dc65fd4cdf
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-31 13:06:07 +0200

    docs: update

--- src/routes/guide/custom-icons/+page.ts
+++ src/routes/guide/custom-icons/+page.ts
@@ -1,4 +1,5 @@
 import type { MetaProps } from 'runes-meta-tags';
+
 const title = 'Custom icons - Svelte Remix'
 const description = 'How to create a custom icons with Svelte Remix'
 const imgUrl = 'https://open-graph-vercel.vercel.app/api/svelte-remix'
