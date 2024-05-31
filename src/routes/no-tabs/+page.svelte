179d9b866009561b255cf2accc3b189f830f851e
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-05-31 13:15:34 +0200

    fix: change ComponentType to Component

--- src/routes/no-tabs/+page.svelte
+++ src/routes/no-tabs/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import type { ComponentType } from 'svelte';
+  import type { Component } from 'svelte';
   import { IconPage, filterStringKeys } from 'runes-webkit'
   import * as icons from '../supertiny-samples'
 
@@ -11,4 +11,4 @@
 minSize = '60'
 defaultSize='80' maxSize='100'
 i_p_div_3='lg:grid-cols-3 md:grid-cols-2 '
-icons={keyIcons as ComponentType} threeTabs={false} title="No Tabs" />
+icons={keyIcons as Component} threeTabs={false} title="No Tabs" />
