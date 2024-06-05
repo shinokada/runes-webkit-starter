edf3e51c1dded697a5f28de2d8698f2a0b5b82fa
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-06-05 17:01:44 +0200

    fix: docs

--- src/routes/guide/svelte-4/getting-started/+page.svelte
+++ src/routes/guide/svelte-4/getting-started/+page.svelte
@@ -56,7 +56,7 @@
 
 <H2>Installation</H2>
 
-<p>Install Svelte and TailwindCSS:</p>
+<p>Install Svelte and Svelte Remix:</p>
 
 <HighlightCompo codeLang="ts" code={modules['./md/installation.md'] as string} />
 
