b9ace2c53a79a17552d3a9115128c454c858ccb9
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-08-25 17:41:03 +0200

    fix: icon to Icon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
   
-  import { removeHyphensAndCapitalize, HomeCards, SupportBanner, TechInfo, BellActiveAltOutline, InfoCircleOutline, cards, info } from 'runes-webkit';
+  import { removeHyphensAndCapitalize, HomeCards, SupportBanner, TechInfo, BellActiveAltOutline, InfoCircleOutline, cards, info, type CardType } from 'runes-webkit';
   import { A } from 'svelte-5-ui-lib';
   const brand = {
     title: `${removeHyphensAndCapitalize(__NAME__)}`,
     description: `${__DESCRIPTION__}`,
-    icon: BellActiveAltOutline,
+    Icon: BellActiveAltOutline,
     icon_class: 'text-blue-500'
   };
 
@@ -14,14 +14,14 @@
   const originalSource = {
     title: 'Original source',
     description: `${__ORIGINAL_SOURCE_NAME__}`,
-    icon: InfoCircleOutline,
+    Icon: InfoCircleOutline,
     href: `${__ORIGINAL_SOURCE_URL__}`,
     icon_class: 'text-orange-500'
   }
   const license = {
     title: 'License',
     description: 'Released under the MIT License.',
-    icon: InfoCircleOutline,
+    Icon: InfoCircleOutline,
     href: `https://github.com/shinokada/${__NAME__}/blob/main/LICENSE`,
     icon_class: 'text-lime-500'
   }
@@ -48,8 +48,8 @@
 
 <h1 class='flex justify-center my-8'>Runes Webkit Starter</h1>
 <h2 class='flex justify-center my-8'><A href ='/how-to-use' class='underline'>This is a demo page. Please read  How to use page.</A> </h2>
-<HomeCards cards={filteredCards}/>
+<HomeCards cards={filteredCards as CardType[]}/>
 <h2 class='flex justify-center my-8'>Info</h2>
-<HomeCards cards={newInfo} />
+<HomeCards cards={newInfo as CardType[]} />
 <TechInfo {...pkg} />
 </div>
